7da65c89e1aabca2b3eaa33a431b3215cfd4d463
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-03-03 18:40:52 +0200

    rename

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Lib1 } from '@webpack-build-libs-from-source-only-works-when-target-name-is-build/lib1';
+import { Lib1 } from '@webpack-no-build-libs-from-source-with-webpack-config/lib1';
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
